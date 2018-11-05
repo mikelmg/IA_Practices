fcb7faf71a2323ceeba0f11b12e63bb45972b0b1
mikelmg <mikelmg_96@hotmail.com>
2018-11-05 12:11:10 +0100

    GoalTest

--- practice 3/rushHourDemo/RushHourGoalTest.java
+++ practice 3/rushHourDemo/RushHourGoalTest.java
@@ -1,7 +1,19 @@
 package rushHourDemo;
 
-import aima.core.agent.State;
+import aima.core.search.framework.problem.GoalTest;
 
-public class RushHourGoalTest implements State {
-	private RushHourState goal = new RushHourState())
+public class RushHourGoalTest implements GoalTest {
+	
+	
+	public boolean isRushHourGoalState(Vehicle redCar, RushHourState state) {
+		return (RushHourState.gatePosition[0] == redCar.getPositionX() && RushHourState.gatePosition[1] == redCar.getPositionY());
+	}
+
+	@Override
+	public boolean isGoalState(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 }
